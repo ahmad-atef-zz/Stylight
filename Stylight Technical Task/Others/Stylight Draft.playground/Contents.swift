c798e49fee878cf9db05f2fb898fb67b3e82614f
Ahmad Atef <ahmad.3atef@gmail.com>
2017-07-20 05:51:39 +0200

    Answered General Questions inside Playground.

--- Stylight Technical Task/Others/Stylight Draft.playground/Contents.swift
+++ Stylight Technical Task/Others/Stylight Draft.playground/Contents.swift
@@ -1,13 +1,29 @@
-import Foundation
+/*:
+ # Hello Stylight!
+ 
+ *This is Ahmad Atef with you...*
+ */
 
-print("Hello, Stylight111")
 
-enum ServerCompass : String{
-    case testing = "testing"
-}
-enum Router {
-    static let baseURL = ServerCompass.testing.rawValue
-}
+/*:
+ [MP3Quran]: https://itunes.apple.com/us/app/mp3quran/id533029431?mt=8 "Some hover text"
 
-print(Router.baseURL)
-print("123123")
+ [ Al-Mushaf]: https://itunes.apple.com/us/app/%D9%85%D8%B5%D8%AD%D9%81-%D8%A7%D9%84%D8%AD%D8%B1%D9%85%D9%8A%D9%86/id1209760565?mt=8 "Some hover text"
+ 
+ [Quran Hakim]: https://itunes.apple.com/us/app/%D8%AA%D8%B1%D8%AC%D9%85%D9%87-%D8%AA%D9%81%D8%B3%DB%8C%D8%B1%DB%8C-%D9%82%D8%B1%D8%A2%D9%86-%D8%AD%DA%A9%DB%8C%D9%85/id1195773263?mt=8 "Some hover text"
+ 
+ 
+ 
+ ### My Applications on App store:
+ > 1. [MP3Quran].
+ > 2. [Al-Mushaf]
+ > 3. [Quran Hakim]
+ 
+ */
+
+
+/*:
+ ### Most challenging problems
+ 
+ *I was using Ads Framework to display iAds based on your current location and this library was killing the battery, and the library was also Locked. So I've to hack in the library and use method swizzling to change the implementation from inside the library. and this was a great challenge and experience for me*
+ */
